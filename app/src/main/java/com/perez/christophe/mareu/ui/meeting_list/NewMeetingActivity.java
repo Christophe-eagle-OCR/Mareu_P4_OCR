213053b7b5ce68f447c9f4df1b89337f398183a0
cperez <chrper31@gmail.com>
2021-09-09 13:00:29 +0200

    1)fait un 1er jet du layout activity_new_meeting.xml , avec uniquement des textField (edit texte) et 1 bouton pour creer la reunion. 2)Dans la class NewMeetingActivity : j'ai utilisé le binding( et pas le findViewById qui est utilisé dans la class MeetingListActivity(main))

--- app/src/main/java/com/perez/christophe/mareu/ui/meeting_list/NewMeetingActivity.java
+++ app/src/main/java/com/perez/christophe/mareu/ui/meeting_list/NewMeetingActivity.java
@@ -1,16 +1,74 @@
 package com.perez.christophe.mareu.ui.meeting_list;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.perez.christophe.mareu.databinding.ActivityNewMeetingBinding;
 
-import com.perez.christophe.mareu.R;
+public class NewMeetingActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class NewMeetingActivity extends AppCompatActivity {
+    ActivityNewMeetingBinding binding;
+
+
+    private void initUI() {
+        binding = ActivityNewMeetingBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        setCreateMeetingBtn();
+        getSupportActionBar().setTitle("Nouvelle réunion");
+    }
+
+    private void setCreateMeetingBtn() {
+        binding.activityNewMeetingCreateMeetingFab.setOnClickListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_meeting);
+        //setContentView(R.layout.activity_new_meeting);
+        initUI();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == binding.activityNewMeetingCreateMeetingFab) {
+            onCreateMeeting();
+        }
+    }
+
+    //todo : manque à creer la methode pour créer la reunion en passant DI et le Meetingrepository
+    private void onCreateMeeting() {
+        String object = binding.addMeetingObjectTextField.getEditText().getText().toString();
+        String date = binding.addMeetingDateTextField.getEditText().getText().toString();
+        String starTime = binding.addMeetingStartTimeTextField.getEditText().getText().toString();
+        String endTime = binding.addMeetingEndTimeTextField.getEditText().getText().toString();
+        String listOfParticipants = binding.addMeetingListOfParticipantsTextField.getEditText().getText().toString();
+        String room = binding.addMeetingRoomTextField.getEditText().getText().toString();
+
+        if (object.isEmpty()) {
+            binding.addMeetingObjectTextField.setError("Merci de préciser l'objet de la réunion");
+            return;
+        }
+        if (date.isEmpty()) {
+            binding.addMeetingDateTextField.setError("Merci de préciser la date de la réunion");
+        }
+        if (starTime.isEmpty()) {
+            binding.addMeetingStartTimeTextField.setError("Merci de préciser l'heure du début de la réunion");
+        }
+        if (endTime.isEmpty()) {
+            binding.addMeetingEndTimeTextField.setError("Merci de préciser l'heure de fin de la réunion");
+        }
+        if (listOfParticipants.isEmpty()){
+            binding.addMeetingListOfParticipantsTextField.setError("Merci de préciser les participants de la réunion");
+        }
+        if (room.isEmpty()){
+            binding.addMeetingRoomTextField.setError("Merci de préciser le nom de la salle de réunion");
+        }
+
+
+
+
     }
 }
