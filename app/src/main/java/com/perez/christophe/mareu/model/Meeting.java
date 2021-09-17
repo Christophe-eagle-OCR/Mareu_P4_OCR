3ca0feccd5b56d2a65a52918c19b4f4f9e1adf15
cperez <chrper31@gmail.com>
2021-09-17 14:27:42 +0200

    créer  "model" (data) pour les  classes : Meeting et Room

--- app/src/main/java/com/perez/christophe/mareu/model/Meeting.java
+++ app/src/main/java/com/perez/christophe/mareu/model/Meeting.java
@@ -1,7 +1,88 @@
 package com.perez.christophe.mareu.model;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Christophe on 03/09/2021.
  */
-public class Meeting {
+public class Meeting implements Serializable {
+
+    private String mObject;
+    //private Date mDate;
+    private String mDate;
+    private String mStartTime;
+    private String mEndTime;
+    //private List<String> mParticipants;
+    private String mParticipants;
+    private Room mRoom;
+
+
+    /**
+     * Constructor
+     * @param object
+     * @param date
+     * @param startTime
+     * @param endTime
+     * @param participants
+     * @param room
+     */
+    public Meeting(String object, String date, String startTime, String endTime, String participants, Room room) {
+        mObject = object;
+        mDate = date;
+        mStartTime = startTime;
+        mEndTime = endTime;
+        mParticipants = participants;
+        mRoom = room;
+    }
+
+    public String getObject() {
+        return mObject;
+    }
+
+    public void setObject(String object) {
+        mObject = object;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public String getStartTime() {
+        return mStartTime;
+    }
+
+    public void setStartTime(String startTime) {
+        mStartTime = startTime;
+    }
+
+    public String getEndTime() {
+        return mEndTime;
+    }
+
+    public void setEndTime(String endTime) {
+        mEndTime = endTime;
+    }
+
+    public String getParticipants() {
+        return mParticipants;
+    }
+
+    public void setParticipants(String participants) {
+        mParticipants = participants;
+    }
+
+    public Room getRoom() {
+        return mRoom;
+    }
+
+    public void setRoom(Room room) {
+        mRoom = room;
+    }
+
 }
--- app/src/main/java/com/perez/christophe/mareu/model/Room.java
+++ app/src/main/java/com/perez/christophe/mareu/model/Room.java
@@ -1,7 +1,52 @@
 package com.perez.christophe.mareu.model;
 
+import java.io.Serializable;
+
 /**
  * Created by Christophe on 03/09/2021.
  */
-public class Room {
+public class Room  implements Serializable {
+
+    private long id;
+    private String mNameOfRoom;
+    private int mColorOfRoom;
+
+
+    /**
+     * Constructor
+     * @param id
+     * @param nameOfRoom
+     * @param colorOfRoom
+     */
+    public Room(long id, String nameOfRoom, int colorOfRoom) {
+        this.id = id;
+        mNameOfRoom = nameOfRoom;
+        mColorOfRoom = colorOfRoom;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNameOfRoom() {
+        return mNameOfRoom;
+    }
+
+    public void setNameOfRoom(String nameOfRoom) {
+        mNameOfRoom = nameOfRoom;
+    }
+
+    public int getColorOfRoom() {
+        return mColorOfRoom;
+    }
+
+    public void setColorOfRoom(int colorOfRoom) {
+        mColorOfRoom = colorOfRoom;
+    }
+
 }
+
