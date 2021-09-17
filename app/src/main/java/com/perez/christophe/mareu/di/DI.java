54d35f3990f2a2a4d50313fd32c7ef937a5b281c
cperez <chrper31@gmail.com>
2021-09-17 14:55:33 +0200

    Créer "di" et classe DI , pour appeller la class MeetingRepositoryImpl en passant par la variable "repository" quand on appèle la methode "getMeetingRepository".

--- app/src/main/java/com/perez/christophe/mareu/di/DI.java
+++ app/src/main/java/com/perez/christophe/mareu/di/DI.java
@@ -1,7 +1,32 @@
 package com.perez.christophe.mareu.di;
 
+import com.perez.christophe.mareu.repository.MeetingRepository;
+import com.perez.christophe.mareu.repository.MeetingRepositoryImpl;
+
 /**
  * Created by Christophe on 03/09/2021.
+ *
+ * Dependence injection to get instance of service
  */
 public class DI {
+
+    private static final MeetingRepository repository = new MeetingRepositoryImpl();
+
+    /**
+     * Get an instance of @{@link MeetingRepository}
+     *
+     * @return
+     */
+    public static MeetingRepository getMeetingRepository() {
+        return repository;
+    }
+
+
+    /**
+     * Get always a new uinstance on @{@link MeetingRepository}.Userful for tests, so we context is clean
+     * @return
+     */
+    public static MeetingRepository getNewInstanceMeetingRepository(){
+        return new MeetingRepositoryImpl();
+    }
 }
